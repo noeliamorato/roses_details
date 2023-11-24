ed2d7daa2a6cc2eac3d3c86d52cd95120752f05e
noeliamorato <moratocaeronoelia123@gmail.com>
2023-11-24 17:07:13 -0400

    24/11/23 sección nosotros

--- client/src/pages/informativa/index.jsx
+++ client/src/pages/informativa/index.jsx
@@ -6,15 +6,31 @@ import {
   Home,
   Nosotros,
   Contactos,
+  Mision,
+  Vision,
+  Valores,
+  Footer,
 } from "../../styles/styled_index";
+
 import { Link } from "react-router-dom";
 import logo from "../../assets/img/logo.png";
+import fondo1 from "../../assets/img/fondo1.png";
+import fondo5 from "../../assets/img/fondo5.png";
+import fondo4 from "../../assets/img/fondo4.png";
+import uno from "../../assets/img/uno.png";
+import dos from "../../assets/img/dos.png";
+import tres from "../../assets/img/tres.png";
+import cuatro from "../../assets/img/cuatro.png";
+import cinco from "../../assets/img/cinco.png";
+import seis from "../../assets/img/seis.png";
 
 const Index = () => {
   return (
     <Container>
       <div className="segundo">
-        <Title>Roses & Details</Title>
+        <Title>
+          <img src={logo} alt="" />
+        </Title>
         <Navbar>
           <ul>
             <li>
@@ -36,9 +52,15 @@ const Index = () => {
       {/* --------------------------------------home----------------------------------------- */}
       <Home>
         <section>
-          <div> Roses & Details</div>
-          <div>
-            <img src={logo} alt="" />
+          <div className="title">
+            <h1 className="roses">Roses</h1>
+            <h1 className="y">&</h1>
+            <h1 className="detalles">Details</h1>
+          </div>
+          <div className="imagenes">
+            <img className="fondo1" src={fondo1} alt="" />
+            <img className="fondo5" src={fondo5} alt="" />
+            <img className="fondo4" src={fondo4} alt="" />
           </div>
         </section>
         <p></p>
@@ -46,18 +68,78 @@ const Index = () => {
 
       {/* --------------------------------------nosotros----------------------------------------- */}
       <Nosotros>
-        <p>hola</p>
+        <Mision>
+          <div>
+            <h1>MISIÓN</h1>
+            <section>
+              <p>
+                En Roses & Details nos comprometemos a proporcionar una
+                experiencia excepcional en compras en línea, ofreciendo una
+                amplia gama de productos únicos y artesanales. Buscamos inspirar
+                la creatividad y la conexión emocional a través de detalles
+                cuidadosamente seleccionados que elevan momentos cotidianos a
+                experiencias extraordinarias para nuestros clientes.
+              </p>
+            </section>
+          </div>
+          <img src={uno} alt="" />
+          <img src={dos} alt="" />
+          <img src={tres} alt="" />
+        </Mision>
+        <Vision>
+          <img src={cuatro} alt="" />
+          <img src={cinco} alt="" />
+          <img src={seis} alt="" />
+          <div>
+            <h1>VISIÓN</h1>
+            <section>
+              <p>
+                Nos esforzamos por ser la principal referencia en el mundo de
+                detalles y manualidades en línea, destacando por nuestra
+                excelencia en calidad, originalidad y atención al cliente.
+                Aspiramos a ser reconocidos como un destino confiable y
+                apreciado, donde la innovación y la pasión por lo artesanal se
+                encuentran para deleitar a nuestros clientes en cada compra.
+              </p>
+            </section>
+          </div>
+        </Vision>
+        <Valores>
+          <div>
+            <h1>VALORES</h1>
+            <section>
+              <p>
+                <p>
+                  1. Creatividad y Originalidad: Nos comprometemos a ofrecer
+                  productos únicos y creativos que inspiren y añadan un toque
+                  especial a cada momento.
+                </p>
+                <p>
+                  2. Calidad y Artesanía: Valoramos la excelencia artesanal y
+                  nos esforzamos por ofrecer productos de la más alta calidad
+                  que transmitan la dedicación de nuestros artesanos.
+                </p>
+                <p>
+                  3. Atención al Cliente: Colocamos a nuestros clientes en el
+                  centro de todo lo que hacemos, ofreciendo un servicio
+                  excepcional y garantizando su satisfacción.
+                </p>
+                <p>
+                  4. Innovación y Mejora Continua: Buscamos constantemente
+                  nuevas formas de mejorar y crecer, adaptándonos a las
+                  necesidades cambiantes del mercado y de nuestros clientes.
+                </p>
+              </p>
+            </section>
+          </div>
+        </Valores>
       </Nosotros>
 
       {/* --------------------------------------contactos----------------------------------------- */}
-      <Contactos>
-        <p>hola</p>
-      </Contactos>
+      <Contactos></Contactos>
 
       {/* --------------------------------------footer----------------------------------------- */}
-      <footer>
-        <p>hola</p>
-      </footer>
+      <Footer></Footer>
     </Container>
   );
 };
--- client/src/styles/styled_index.jsx
+++ client/src/styles/styled_index.jsx
@@ -1,12 +1,13 @@
 import styled from "styled-components";
 
-export const color = {
+export const colors = {
   padrecolor: "#ffff",
   primary: "#efd3d4",
   secondary: "#b01a1d",
   terceary: "#5f1b1e",
   color: "#263f30",
-}
+  black: "#000",
+};
 
 export const Container = styled.section`
   .segundo {
@@ -18,12 +19,12 @@ export const Container = styled.section`
 `;
 
 export const Title = styled.div`
-  color: ${color.secondary};
   display: flex;
   align-items: center;
   justify-content: left;
-  padding: 0.5em;
-  font-size: 30px;
+  & > img {
+    width: 180px;
+  }
 `;
 
 export const Navbar = styled.nav`
@@ -31,6 +32,7 @@ export const Navbar = styled.nav`
   display: flex;
   justify-content: left;
   padding-left: 20em;
+
   ul {
     display: flex;
     flex-direction: row;
@@ -43,52 +45,232 @@ export const Navbar = styled.nav`
   }
   a {
     text-decoration: none;
-    color: ${color.terceary};
+    color: ${colors.terceary};
   }
 `;
 
 export const Home = styled.section`
-position: relative;
-width: 100vw;
-height: 100vh;
-&::before{ //seudoclase que permite crear un elemento, es decir: crea una caja igual a la otra osea home
-  background-color: ${color.secondary};
-  content: ''; //contenido que tiene la caja copiada 
-  position: absolute;
-  width: 30%;
-  height: 100vh;
-  right: 0;
-  z-index: -1;
-}
-
-&::after{
-  background-color: ${color.primary};
-  content: '';
-  position: absolute;
-  width: 70%;
-  height: 100vh;
-  left: 0;
-  top: 0;
-  z-index: -1;
-}
-
-& > section{
+  position: relative;
   width: 100vw;
   height: 100vh;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
+  &::before {
+    //seudoclase que permite crear un elemento, es decir: crea una caja igual a la otra osea home
+    background-color: ${colors.secondary};
+    content: ""; //contenido que tiene la caja copiada
+    position: absolute;
+    width: 30%;
+    height: 100vh;
+    right: 0;
+    z-index: -1;
+  }
 
-  & > div{
-    & > img{
-      width: 500px;
+  &::after {
+    background-color: ${colors.primary};
+    content: "";
+    position: absolute;
+    width: 70%;
+    height: 100vh;
+    left: 0;
+    top: 0;
+    z-index: -1;
+  }
+
+  & > section {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    .imagenes {
+      background-color: #eeecef;
+      width: 550px;
+      height: 400px;
+      display: flex;
+      flex-direction: center;
+      align-items: center;
+      justify-content: center;
+      .fondo1 {
+        width: 240px;
+        position: absolute;
+        top: 75%;
+        left: 60%;
+        transform: translate(-100%, -100%);
+        z-index: 1;
+      }
+      .fondo4 {
+        width: 220px;
+        position: absolute;
+        top: 90%;
+        left: 65%;
+        transform: translate(-100%, -100%);
+        z-index: 1;
+      }
+      .fondo5 {
+        width: 270px;
+        position: absolute;
+        top: 75%;
+        left: 80%;
+        transform: translate(-100%, -100%);
+        z-index: 1;
+      }
+    }
+
+    .title {
+      .roses {
+        font-size: 110px;
+        color: ${colors.secondary};
+        display: flex;
+        flex-direction: center;
+        justify-content: center;
+        align-items: center;
+        position: absolute;
+        top: 40%;
+        left: 45%;
+        transform: translate(-100%, -100%);
+        z-index: 1;
+      }
+      .y {
+        font-size: 90px;
+        color: ${colors.secondary};
+        display: flex;
+        flex-direction: center;
+        justify-content: center;
+        align-items: center;
+        position: absolute;
+        top: 55%;
+        left: 40%;
+        transform: translate(-100%, -100%);
+        z-index: 1;
+      }
+      .detalles {
+        color: transparent;
+        -webkit-text-stroke: 1px ${colors.secondary};
+        font-size: 90px;
+        position: relative;
+        top: 70px;
+      }
     }
   }
-}
-
 `;
+//-----------------------------------------NOSOTROS----------------------------------------------------------
+export const Nosotros = styled.section`
+  background-color: ${colors.padrecolor};
+  width: 100vw;
+  height: 200vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-top: 1em;
+  gap: 1em;
+`;
+export const Mision = styled.div`
+  background-color: #eeecef;
+  width: 100vw;
+  height: 350px;
+  display: flex;
+  flex-direction: row;
 
-export const Nosotros = styled.section``;
-
-export const Contactos = styled.section``;
+  & > div {
+    & > section {
+      margin: 1em;
+      background-color: ${colors.primary};
+      width: 400px;
+      height: 200px;
+      border-radius: 50px;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: center;
+      & > p {
+        width: 300px;
+      }
+    }
+    & > h1 {
+      color: transparent;
+      -webkit-text-stroke: 1px ${colors.black};
+      font-size: 40px;
+      margin-inline-start: 3em;
+      margin-top: 0.5em;
+    }
+  }
+  & > img {
+    width: 280px;
+    height: 300px;
+  }
+`;
+export const Vision = styled.div`
+  background-color: #eeecef;
+  width: 100vw;
+  height: 300px;
+  display: flex;
+  flex-direction: row;
+  justify-content: end;
+  align-items: end;
+  & > div {
+    & > section {
+      margin: 1em;
+      background-color: ${colors.primary};
+      width: 400px;
+      height: 200px;
+      border-radius: 50px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      & > p {
+        width: 300px;
+      }
+    }
+    & > h1 {
+      color: transparent;
+      -webkit-text-stroke: 1px ${colors.black};
+      font-size: 40px;
+      margin-inline-end: 5em;
+    }
+  }
+  & > img{
+    width: 200px;
+    height: 300px;
+  }
+`;
+export const Valores = styled.div`
+  background-color: #eeecef;
+  width: 100vw;
+  height: 500px;
+  & > div {
+    & > section {
+      margin: 1em;
+      background-color: ${colors.primary};
+      width: 600px;
+      height: 300px;
+      border-radius: 50px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      & > p {
+        width: 500px;
+      }
+    }
+    & > h1 {
+      color: transparent;
+      -webkit-text-stroke: 1px ${colors.black};
+      font-size: 40px;
+      margin-inline-start: 4em;
+      margin-top: 0.5em;
+    }
+  }
+`;
+//-------------------------------------CONTACTOS------------------------------------------------------------
+export const Contactos = styled.section`
+  background-color: ${colors.secondary};
+  width: 100vw;
+  height: 100vh;
+  margin-top: 1em;
+`;
+//-------------------------------------FOOTER---------------------------------------------------------------
+export const Footer = styled.section`
+  background-color: ${colors.terceary};
+  width: 100vw;
+  height: 50vh;
+  margin-top: 1em;
+`;
